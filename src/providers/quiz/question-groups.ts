e8e7d0bdd6347de90af95d1c860d74d750d59a90
Andrew Ettenger <ettenger@gmail.com>
2017-11-17 00:06:37 -0500

    Add questions

--- src/providers/quiz/question-groups.ts
+++ src/providers/quiz/question-groups.ts
@@ -1,11 +1,82 @@
 export const QUESTION_GROUPS = {
   'taste in pizza toppings': {
+    tagline: 'What slice of pie you like?',
+    questions: [
+      {
+        question: 'Favorite meat?',
+        answers: [
+          {
+            name: 'Pepperoni',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Blondie%27s_pepperoni_pizza_slice_2.JPG'
+          },
+          {
+            name: 'Sausage',
+            image: 'https://cdn.pixabay.com/photo/2015/10/17/17/50/pizza-992935_960_720.jpg'
+          },
+          {
+            name: 'Bacon',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Thin-crust_tortilla_pizza_with_bacon%2C_onions%2C_peppers_a_la_%40seriouseats_%2816534794269%29.jpg'
+          },
+          {
+            name: 'Ham',
+            image: 'https://c.pxhere.com/photos/8c/79/pizza_prosciutto_pizza_dinner_delicious_crust_food_eat_ham-1015051.jpg!d'
+          }
+        ]
+      },
+      {
+        question: 'Favorite combo?',
+        answers: [
+          {
+            name: 'Hawaiian',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/d/d1/Hawaiian_pizza_1.jpg'
+          },
+          {
+            name: 'Veggie Lovers',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Vegan_Pizza_Crust_%285753189490%29.jpg'
+          },
+          {
+            name: 'Meat Lovers',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Meat_lover%27s_pizza_%2816803723523%29.jpg'
+          },
+          {
+            name: 'Mushroom, pepperoni, and bell pepper',
+            image: 'http://res.freestockphotos.biz/pictures/16/16523-illustration-of-a-slice-of-pizza-with-toppings-pv.png'
+          }
+        ]
+      }
+    ]
 
   },
   'favorite Backstreet Boy': {
+    tagline: 'Your one desire',
+    questions: [
+      {
+        question: 'Who is your favorite?',
+        answers: [
+          {
+            name: 'AJ',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/d/d7/AJ_Malibu_003.jpg'
+          },
+          {
+            name: 'Howie',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Howie_singing_2.jpg'
+          },
+          {
+            name: 'Nick',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/6/67/Backstreet_Boys_Concert_-_Nick_Carter_Singing_Incomplete.jpg'
+          },
+          {
+            name: 'Kevin',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/f/f2/Kevin_Richardson_on_tour.jpg'
+          },
+          {
+            name: 'Brian',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/BrianLittrell.jpg'
+          },
+        ]
+      }
+    ]
 
   },
-  'favorite cocktail': {
-    
-  }
+  // 'favorite cocktail': {}
 }
